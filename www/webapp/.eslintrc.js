c30750c99ce6afd25298076107b0bc68d044711b
Rotzbua <Rotzbua@users.noreply.github.com>
2023-07-11 17:12:25 +0200

    rule
    rule

--- www/webapp/.eslintrc.js
+++ www/webapp/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    // 'plugin:vue/strongly-recommended',
+    'plugin:vue/strongly-recommended',
     // 'plugin:vue/recommended',
     'plugin:vuetify/base',
     'eslint:recommended'
@@ -17,6 +17,38 @@ module.exports = {
     'vue/v-bind-style': 'warn',
     'vue/v-on-style': 'warn',
     'vue/v-slot-style': 'warn',
+    'vue/first-attribute-linebreak': 'off',
+    'vue/html-indent': 'off', // TODO
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/mustache-interpolation-spacing': ['warn', 'always'],
+    'vue/no-lone-template': 'warn',
+    'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': ['warn', {
+      'startTag': 'never',
+      'endTag': 'never',
+      'selfClosingTag': 'never',
+    }],
+    'vue/html-self-closing': ['warn', {
+      'html': {
+        'void': 'never',
+        'normal': 'any',
+        'component': 'any',
+      },
+    }],
+    'vue/require-default-prop': 'off', // TODO
+    'vue/this-in-template': ['warn', 'never'],
+    // "vue/component-tags-order":"warn",
+    // "vue/component-tags-order": ["warn", {
+    //   "order": [
+    //     "template",
+    //     "style",
+    //     "script",
+    //   ]
+    // }],
+    // "vue/order-in-components": "off"
   },
   ignorePatterns: ['**/src/modules/**/*'],
   parserOptions: {
