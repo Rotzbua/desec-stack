8f5e107c76d58955d92122f5c618ea75a1a5ce79
Rotzbua <Rotzbua@users.noreply.github.com>
2023-09-27 19:20:03 +0200

    Add API proxy for testing in development
    https://vitejs.dev/config/server-options.html#server-proxy

--- www/webapp/vite.config.js
+++ www/webapp/vite.config.js
@@ -1,29 +1,45 @@
 /* eslint-env node */
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import {resolve} from 'node:path';
 import Components from 'unplugin-vue-components/vite'
 import {VuetifyResolver} from 'unplugin-vue-components/resolvers'
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue2'
 
-export default defineConfig({
-    define: {
-        'process.env.BUILD': '"web"' // fix for vuelidate@0.7.7
-    },
-    plugins: [
-        vue(),
-        Components({
-            resolvers: [VuetifyResolver()],
-        }),
-        legacy(), // Build for old browser.
-    ],
-    server: {
-        port: 8080,
-    },
-    resolve: {
-        alias: [{
-            find: '@', replacement: resolve(__dirname, 'src')
-        }],
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-    },
+const defaultAPIUrl = 'http://localhost';
+
+export default defineConfig(({command, mode, ssrBuild}) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    // https://main.vitejs.dev/config/#using-environment-variables-in-config
+    const env = loadEnv(mode, process.cwd(), '')
+    const activeAPIUrl = env.VITE_APP_TARGET_API ?? defaultAPIUrl;
+    return {
+        define: {
+            'process.env.BUILD': '"web"' // fix for vuelidate@0.7.7
+        },
+        plugins: [
+            vue(),
+            Components({
+                resolvers: [VuetifyResolver()],
+            }),
+            legacy(), // Build for old browser.
+        ],
+        server: {
+            port: 8080,
+            proxy: {
+                '/api': {
+                    target: activeAPIUrl,
+                    changeOrigin: true,
+                    secure: false,
+                }
+            },
+        },
+        resolve: {
+            alias: [{
+                find: '@', replacement: resolve(__dirname, 'src')
+            }],
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+        }
+    }
 })
